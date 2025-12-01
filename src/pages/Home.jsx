8c892c506c279d6e81d237c0e6965849fd7d72c6
sankarjyoti <sankarjyotichetia57@gmail.com>
2025-12-01 15:52:26 +0530

    dg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -66,7 +66,7 @@ function Home() {
                   don’t hesitate to reach out. Let’s create something amazing
                   together!
                   <br/>
-                  DM me for a free homepage audit.
+                  DM ME FOR A FREE HOMEPAGE AUDIT...
                 </p>
                 <div className="flex sm:gap-x-3 gap-y-3 sm:gap-y-0 flex-col sm:flex-row">
                   <button className="connectBTN px-6 py-3 rounded-[20px] border-[1px] border-[var(--white-secondary)] z-10 text-[var(--white-primary)] font-semibold tracking-[0.5px] text-[12px] hover:transform">
